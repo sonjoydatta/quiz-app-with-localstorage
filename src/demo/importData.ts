e73619c1210df0800247e3aa86089889f81b1857
Sonjoy Datta <sonjoy.dat@gmail.com>
2021-08-28 09:38:47 +0600

    Added GitHub pages configration

--- src/demo/importData.ts
+++ src/demo/importData.ts
@@ -6,9 +6,12 @@ export const importData = (id: number, setStart: Dispatch<SetStateAction<number>
 	const isData = localStorage.getItem('dummyData');
 
 	if (isData !== 'YES') {
-		if (dummyQuizzes[id]) quiz.create(dummyQuizzes[id]);
 		if (dummyMembers[id]) user.create(dummyMembers[id]);
-		if (!dummyQuizzes[id] && !dummyMembers[id]) localStorage.setItem('dummyData', 'YES');
+		if (dummyQuizzes[id]) {
+			quiz.create(dummyQuizzes[id]);
+		} else {
+			localStorage.setItem('dummyData', 'YES');
+		}
 		setStart(id + 1);
 	}
 };
